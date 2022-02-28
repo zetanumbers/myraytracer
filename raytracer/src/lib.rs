3cca229c82cf89e1153925c8b74bac4042717e1c
Daria Sukhonina <dariasukhonina@gmail.com>
2022-03-01 01:32:30 +0300

    Normal sphere

--- raytracer/src/lib.rs
+++ raytracer/src/lib.rs
@@ -12,16 +12,8 @@ impl Ray {
     }
 
     pub fn at(self, t: f32) -> Vec3 {
-        self.origin * t + self.direction
+        self.origin + self.direction * t
     }
-
-    fn hit(self, visible: &impl Hit) -> bool {
-        visible.hit_with_ray(self)
-    }
-}
-
-trait Hit {
-    fn hit_with_ray(&self, ray: Ray) -> bool;
 }
 
 pub struct World {
@@ -41,8 +33,8 @@ impl Default for World {
 
 impl World {
     pub fn color(&self, ray: Ray) -> Vec4 {
-        if ray.hit(self) {
-            return vec4(1., 0., 0., 1.);
+        if let Some(r) = ray.hit(self) {
+            return Vec4::from((0.5 * (r.normal + Vec3::ONE), 1.));
         }
 
         let t = 0.5 * (ray.direction.normalize_or_zero().y + 1.);
@@ -50,9 +42,25 @@ impl World {
     }
 }
 
+trait Hit {
+    fn hit_with_ray(&self, ray: Ray) -> Option<HitReport>;
+}
+
+struct HitReport {
+    t: f32,
+    at: Vec3,
+    normal: Vec3,
+}
+
+impl Ray {
+    fn hit(self, visible: &impl Hit) -> Option<HitReport> {
+        visible.hit_with_ray(self)
+    }
+}
+
 impl Hit for World {
-    fn hit_with_ray(&self, ray: Ray) -> bool {
-        self.spheres.iter().any(|s| ray.hit(s))
+    fn hit_with_ray(&self, ray: Ray) -> Option<HitReport> {
+        self.spheres.iter().find_map(|s| ray.hit(s))
     }
 }
 
@@ -63,12 +71,20 @@ pub struct Sphere {
 }
 
 impl Hit for Sphere {
-    fn hit_with_ray(&self, ray: Ray) -> bool {
+    fn hit_with_ray(&self, ray: Ray) -> Option<HitReport> {
         let oc = ray.origin - self.center;
         let a = ray.direction.length_squared();
         let b = oc.dot(ray.direction);
         let c = oc.length_squared() - self.radius.powi(2);
         let d = b.powi(2) - a * c;
-        d > 0.
+
+        let t = (d >= 0.).then(|| (-b - d.sqrt()) / a)?;
+        if t < 0.0 {
+            return None;
+        }
+        let at = ray.at(t);
+        let normal = (at - self.center) / self.radius;
+
+        Some(HitReport { t, at, normal })
     }
 }
